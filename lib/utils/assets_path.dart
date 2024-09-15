7719635b513f00bf9b7ad1e735f28a4594fc953e
abdifatahmuse <cabdifataaxmuuse@gmail.com>
2024-09-15 15:05:34 +0300

    :sparkles: Utils

---
+++ lib/utils/assets_path.dart
@@ -0,0 +1,40 @@
+import 'package:kulmiye/main_paths.dart';
+
+class AssetsPath {
+  // Images
+  static String logo = 'logo'.pngIcon;
+
+  // Icons
+  static String home = 'home'.pngIcon;
+
+  // SVG
+  static String homeSvg = 'home'.svgIcon;
+
+  // Fonts
+  static String robotoRegular = 'assets/fonts/Poppins-Regular.ttf';
+  static String robotoBold = 'assets/fonts/Poppins-Bold.ttf';
+  static String robotoMedium = 'assets/fonts/Poppins-Medium.ttf';
+  static String robotoLight = 'assets/fonts/Poppins-Light.ttf';
+  static String robotoThin = 'assets/fonts/Poppins-Thin.ttf';
+  static String robotoItalic = 'assets/fonts/Poppins-Italic.ttf';
+
+  // // JSON
+  // static String countries = 'assets/json/countries.json';
+
+  // // Videos
+  // static String introVideo = 'assets/videos/intro.mp4';
+  // static String introVideo2 = 'assets/videos/intro2.mp4';
+
+  // // Audios
+  // static String introAudio = 'assets/audios/intro.mp3';
+
+  // // Lottie
+  // static String lottie = 'assets/lottie/animation.json';
+
+  // // Others
+  // static String pdf = 'assets/pdf/sample.pdf';
+  // static String html = 'assets/html/sample.html';
+  // static String txt = 'assets/txt/sample.txt';
+  // static String csv = 'assets/csv/sample.csv';
+  // static String xls = 'assets/xls/sample.xls';
+}
